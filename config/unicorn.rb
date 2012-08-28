c692a9187a00ba4d53e56b0a496752122c664c64
Joshua Mervine <joshua@mervine.net>
2012-08-28 12:59:52 -0700

    adding real unicorn configs

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,6 @@
 timeout 30
-
+worker_processes 2
+stderr_path File.join(File.dirname(__FILE__), "..", "..", "shared", "log", "unicorn_err.log")
+stdout_path File.join(File.dirname(__FILE__), "..", "..", "shared", "log", "unicorn_out.log")
+stdout_path File.join(File.dirname(__FILE__), "..", "..", "shared", "pids", "unicorn_out.log")
+listen 8080
