c06547e1a066208dd719a94d9f2e79b5daec8188
Joshua Mervine <joshua@mervine.net>
2012-08-28 13:56:30 -0700

    tweaking unicorn for my server

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,9 @@
 timeout 30
 worker_processes 2
-stderr_path File.join(File.dirname(__FILE__), "..", "..", "shared", "log", "unicorn_err.log")
-stdout_path File.join(File.dirname(__FILE__), "..", "..", "shared", "log", "unicorn_out.log")
-stdout_path File.join(File.dirname(__FILE__), "..", "..", "shared", "pids", "unicorn_out.log")
-listen 8080
+
+shared = "/home/jmervine/Sites/shared/"
+stderr_path File.join(shared, "log", "unicorn_err.log")
+stdout_path File.join(shared, "log", "unicorn_out.log")
+stdout_path File.join(shared, "pids", "unicorn_out.log")
+
+isten 8080
