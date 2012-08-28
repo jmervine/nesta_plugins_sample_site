105d6b37faacb9d85eb41a4e6480ce3eca5ef6f2
Joshua Mervine <joshua@mervine.net>
2012-08-28 12:40:25 -0700

    fixing unicorn command for vlad

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,5 +2,5 @@ set :application, "sample"
 set :repository, "https://github.com/jmervine/nesta_plugins_sample_site"
 set :domain, "home.mervine.net"
 set :deploy_to, "/home/jmervine/Sites"
-
+set :unicorn_command, "bundle exec unicorn"
 
